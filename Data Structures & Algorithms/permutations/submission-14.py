792567b5f8421990e607a672db8e46d36f3c2bd4
rhahudson1 <132921507+rhahudson1@users.noreply.github.com>
2026-08-22 18:37:32 -0400

    Add: permutations - submission-14

---
+++ Data Structures & Algorithms/permutations/submission-14.py
@@ -0,0 +1,22 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+        used = [False] * len(nums)
+
+        def dfs():
+            if len(subset) == len(nums):
+                res.append(subset.copy())
+                return
+            for i in range(len(nums)):
+                if not used[i]:
+                    subset.append(nums[i])
+                    used[i] = True
+                    dfs()
+                    subset.pop()
+                    dfs()
+                    used[i] = False
+        dfs()
+        return res
+            
+        
