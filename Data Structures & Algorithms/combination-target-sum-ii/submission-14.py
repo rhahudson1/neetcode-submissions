6c0f609feede3191706f48738cf2d55c20a0ad87
rhahudson1 <132921507+rhahudson1@users.noreply.github.com>
2026-08-22 18:02:27 -0400

    Add: combination-target-sum-ii - submission-14

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-14.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        res = []
+        subset = []
+        def dfs(i, cur):
+            if cur == target: 
+                res.append(subset.copy())
+                return
+            if i == len(candidates) or cur > target:
+                return
+            subset.append(candidates[i])
+            dfs(i+1, cur + candidates[i])
+            subset.pop()
+            while i + 1 < len(candidates) and candidates[i] == candidates[i+1]:
+                i += 1
+            dfs(i+1, cur)
+        dfs(0,0)
+        return res
+            
+
+        
