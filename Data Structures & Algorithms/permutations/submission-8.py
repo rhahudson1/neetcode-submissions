74cea9bdb37f6a0be190c107530e728288f2cad7
rhahudson1 <132921507+rhahudson1@users.noreply.github.com>
2026-08-22 12:28:56 -0400

    Add: permutations - submission-8

---
+++ Data Structures & Algorithms/permutations/submission-8.py
@@ -0,0 +1,16 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+        used = [False] * len(nums)
+        def dfs():
+            if len(subset) == len(nums):
+                res.append(subset.copy())
+                return
+            for i in range(len(nums)):
+                if not user[i]:
+                    subset.append(nums[i])
+                    used[i] = True
+        dfs()
+        return res
+                
