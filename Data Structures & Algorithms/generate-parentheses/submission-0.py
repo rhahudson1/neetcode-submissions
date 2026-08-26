2e883ed359286ce2e2bbf5ed933f14112b90751a
rhahudson1 <132921507+rhahudson1@users.noreply.github.com>
2026-08-26 17:32:40 -0400

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        # can add ( only if you still have openings left (open < n)
+        # can add ) only if it won't break validdity (close < open)
+        # A string is complete and valid only when open == close == n
+        stack = []
+        res = []
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
+                return
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+            if closedN < openN:
+                stack.appnend(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+
+            
+        backtrack(0,0)
+        return res
+
+                
