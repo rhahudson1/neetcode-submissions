931c167f4c9bcade6b37abbc36f564de4d6713f5
rhahudson1 <132921507+rhahudson1@users.noreply.github.com>
2026-08-31 15:22:29 -0400

    Add: coin-change - submission-32

---
+++ Data Structures & Algorithms/coin-change/submission-32.py
@@ -0,0 +1,17 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        cache = [float("inf")] * (amount+1)
+        cache[0] = 1
+        for i in range(1,amount+1):
+          for coin in coins:
+            if amount - coin >= 0:
+                cache[i] = min(cache[i], 1 + cache[i-coin])
+        res = cache[-1]
+        if res == float("inf"):
+            return -1
+        return res
+            
+
+
+        
+        
