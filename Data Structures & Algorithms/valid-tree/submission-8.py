b568b66b3d2c465eb2df7c6562d201c2d103895d
rhahudson1 <132921507+rhahudson1@users.noreply.github.com>
2026-09-02 15:02:37 -0400

    Add: valid-tree - submission-8

---
+++ Data Structures & Algorithms/valid-tree/submission-8.py
@@ -0,0 +1,28 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # connected
+        # no cycles
+        if len(edges) > (n - 1):
+            return False
+
+        adj = [[] for _ in range(n)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        
+        visitSet = set()
+        def dfs(i, par):
+            if i in visitSet:
+                return False
+            visitSet.add(i)
+            for nei in adj[i]:
+                if nei == par:
+                    continue
+                if not dfs(nei, i):
+                    return False
+            visitSet.remove(i)
+            return True
+        return dfs(0,-1) and len(visit) == n
+
+
+        
