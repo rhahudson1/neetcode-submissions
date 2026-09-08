381b7bdbd0ae339c4d02360b4fd61b5bf67ec473
rhahudson1 <132921507+rhahudson1@users.noreply.github.com>
2026-09-08 14:14:56 -0400

    Add: house-robber-ii - submission-30

---
+++ Data Structures & Algorithms/house-robber-ii/submission-30.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+    # thing about a circle
+    # you can rob the first house but not include the last house
+    # can not rob the first house but can include the last hosue
+    # try algo from nums[1:] or nums[:-1]
+        def maxValue(nums):
+            cache = [-1] * len(nums)
+            def dfs(i):
+                if i >= len(nums):
+                    return 0
+                if cache[i] != -1:
+                    return cache[i]
+                cache[i] = max(nums[i] + dfs(i+2), dfs(i+1))
+                return cache[i]
+            return cache[0]
+        return max(maxValue(nums[1:]), maxValue(nums[:-1]))
+        
