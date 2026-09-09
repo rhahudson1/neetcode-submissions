ecb4632e9f8bf28d7d1bcc56fbe8b7eee0c6b401
rhahudson1 <132921507+rhahudson1@users.noreply.github.com>
2026-09-09 16:30:02 -0400

    Add: course-schedule-ii - submission-8

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-8.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        pre = defaultdict(list)
+        for crs, pr in prerequisites:
+            pre[crs].append(pr)
+        res = []
+        curPath = set()
+        doneSet = set()
+        def dfs(i):
+            if i in curPath:
+                return False
+            if i in doneSet:
+                return True
+            curPath.add(i)
+            for nei in pre[i]:
+                if not dfs(nei):
+                    return False
+            curPath.remove(i)
+            doneSet.add(i)
+            res.append(i)
+            return True
+
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
+        return res
+        
