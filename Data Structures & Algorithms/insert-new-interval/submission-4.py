73bd90bb0c8c012bf857f8e6801194cf441fb4f6
rhahudson1 <132921507+rhahudson1@users.noreply.github.com>
2026-09-09 12:18:32 -0400

    Add: insert-new-interval - submission-4

---
+++ Data Structures & Algorithms/insert-new-interval/submission-4.py
@@ -0,0 +1,6 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        # iterate through 
+        intervals.sort(lambda key: x[0])
+        print(intervals)
+        
