faed061b5c8e258b588f7f11786fb0f060a5d193
rhahudson1 <132921507+rhahudson1@users.noreply.github.com>
2026-09-09 15:09:30 -0400

    Add: pacific-atlantic-water-flow - submission-6

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-6.py
@@ -0,0 +1,13 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        rows, cols = len(grid), len(grid[0])
+        pac = set()
+        atl = set()
+        for r in range(rows):
+            if heights[r][0]:
+                pac.add((r,0))
+            if heights[r][cols-1]:
+                alt.add((r,cols-1))
+        print(pac)
+
+        
