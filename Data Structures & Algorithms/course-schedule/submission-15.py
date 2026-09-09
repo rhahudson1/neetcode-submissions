8d2c93498cb0d0ddbf4fdf9b04f09171832e0c23
rhahudson1 <132921507+rhahudson1@users.noreply.github.com>
2026-09-09 15:41:24 -0400

    Add: course-schedule - submission-15

---
+++ Data Structures & Algorithms/course-schedule/submission-15.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = defaultdict(list)
+        for pre,crs in prerequisites:
+            adj[crs].append(crs)
+        curPath = set()
+        def dfs(i):
+            if i in curPath:
+                return False
+            if adj[i] == []:
+                return True
+            curPath.add(i)
+            for pre in adj[i]:
+                if not dfs(pre):
+                    return False
+            curPath.remove(i)
+            adj[i] = []
+            return True
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        return True
+
+
+        
