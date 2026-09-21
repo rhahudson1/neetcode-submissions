2383baed316b20d9458f136f7fa2b1e8f51ef4bc
rhahudson1 <132921507+rhahudson1@users.noreply.github.com>
2026-09-21 13:23:54 -0400

    Add: target-sum - submission-2

---
+++ Data Structures & Algorithms/target-sum/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        # you can either subtract or add the number to the total
+        # return the number of different ways that you can build the epxression to ttoal 
+        cache = {}
+        def dfs(i,total):
+            if i >= len(nums):
+                return 0
+            if i == len(nums)-1 and total == target:
+                return 1
+            cache[(i,total)] = dfs(i+1,total - nums[i]) + dfs(i+1, total + nums[i])
+            return cache[(i,total)]
+        return dfs(0,0)
+        
