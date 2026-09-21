9093b57d4cb08cf58deb666d4245f4ccca8452e9
rhahudson1 <132921507+rhahudson1@users.noreply.github.com>
2026-09-21 12:11:55 -0400

    Add: buy-and-sell-crypto-with-cooldown - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # cool down of one day per neetcoin
+        # Complete as many transacatioons as you like
+        # [ 1 3 4 0 4]
+        # 1
+        # 
+        dp = {} # key=(i, buying) val = max_profit
+        def dfs(i, buying):
+            if i >= len(prices):
+                return 0 
+            if (i, buying) in dp:
+                return dp[(i,buying)]
+            if buying:
+                buy = dfs(i+1, not buying) - price[i]
+                cooldown = dfs(i+1, buying)
+                dp[(i,buying)] = max(buy, cooldown)
+            if selling:
+                sell = dfs(i+2, not buying) + prices[i]
+                cooldown = dfs(i+1, buying)
+                dp[(i,buying)] = max(sell, cooldown)
+            return dp[(i,buying)]
+        return dfs(0,True)
+
+
+
+
+
+
+
+        
+        
