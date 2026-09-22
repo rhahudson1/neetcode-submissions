33bc8ca58b763fd72061a3e959413ca1ede16025
rhahudson1 <132921507+rhahudson1@users.noreply.github.com>
2026-09-21 20:06:59 -0400

    Add: edit-distance - submission-5

---
+++ Data Structures & Algorithms/edit-distance/submission-5.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        # can only do one operation
+        # insert a character at any position
+        # delete a character at any position
+        # replace a achracteer at any position
+
+        # return min number of operations to make word1 equal to word 2
+        cache = [[float("inf")] * (len(word2) + 1) for i in range(len(word1)+ 1)]
+        #    w o r d 2
+        # w
+        # o
+        # r 
+        # d
+        # 1
+        # dp[i][j] = the minimum number of operations to convert word1[i:] into word2[j:]
+        for i in range(len(word1)+1):
+            cache[len(word2)][i] = len(word2) - i 
+        for j in range(len(word2)+1):
+            cache[j][len(word1)] = len(word1) - j
+        # initialize the base case of the array
+
+        for i in range(len(word1)-1,-1,-1):
+            for j in range(len(word2) - 1, -1,-1):
+                if word1[i] == word2[j]:
+                    cache[i][j] = cache[i+1][j+1]
+                else:
+                    cache[i][j] = 1 + min(cache[i+1][j], cache[i][j+1], cache[i+1][j+1])     # those three options
+        return cache[0][0]
+
+
+
+
+        
